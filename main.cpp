eb4d21e29743afa5fc92d76c13aa083427b489eb
ldh0906 <a30308104@gmail.com>
2026-09-07 10:15:46 +0900

    202302590 이동하

--- main.cpp
+++ main.cpp
@@ -2,14 +2,17 @@
 
 using namespace std;
 
-// 학번:
-// 이름:
+// 학번: 202302590
+// 이름: 이동하
 
 int main(int argc, const char *argv[]) {
     // TODO: 아래 두 줄을 정확히 출력하도록 코드를 작성하세요.
     //
     //   Hello, OOP!
     //   C++ is ready.
+    
+    std::cout << "Hello, OOP!" << std::endl;
+    std::cout << "C++ is ready." << std::endl;
 
     return 0;
 }
